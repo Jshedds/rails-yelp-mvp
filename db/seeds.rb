a151c0025d0b18313795ebe0feb0f88b456b53f5
Jamie Sheddan <jsheddan@outlook.com>
2023-11-22 16:05:57 +0000

    half done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,15 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
+Restaurant.destroy_all
+dishoom = { name: 'Dishoom', address: '7 Boundary Street, London E2 7JE', phone_number: '007', category: 'chinese' }
+new_xian = { name: 'New Xian', address: 'Morningside, Edinburgh', phone_number: '001', category: 'chinese' }
+cafe_andeluz = { name: 'Cafe Andeluz', address: 'George Street, Edinburgh', phone_number: '003', category: 'belgian' }
+delivino = { name: 'Delivino', address: 'King street, Crieff', phone_number: '002', category: 'italian' }
+petit_paris = { name: 'Petit Paris', address: 'GrassMarket, Edinburgh', phone_number: '004', category: 'french' }
+
+[dishoom, new_xian, cafe_andeluz, delivino, petit_paris].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
 #   end
